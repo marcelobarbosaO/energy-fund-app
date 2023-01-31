49df098c93afbe0627c084b4e9fcb9ade9287861
Marcelo Barbosa <t_marcelo.barbosa@hapvida.com.br>
2023-01-31 15:47:33 -0300

    refactor: create canBack button, title and subtitle

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,42 @@
+import { MaterialIcons } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+
+import { Typography } from '#components';
+
 import * as S from './styles';
 
-const Header = ({ children }: { children?: any }) => (
-  <S.Container>{children || null}</S.Container>
-);
+interface HeaderProps {
+  canBack?: boolean;
+  title?: string;
+  subTitle?: string;
+}
+
+const Header = ({ canBack, subTitle, title }: HeaderProps) => {
+  if (!canBack && !title) return <S.Container />;
+
+  const navigation = useNavigation();
+
+  return (
+    <S.Container>
+      {canBack ? (
+        <S.ButtonIcon onPress={() => navigation.goBack()}>
+          <MaterialIcons name="arrow-back" color="#000" size={30} />
+        </S.ButtonIcon>
+      ) : null}
+      <S.CenterContent>
+        {title ? (
+          <Typography fontSize={17} bold>
+            {title}
+          </Typography>
+        ) : null}
+        {subTitle ? (
+          <Typography fontSize={14} grey>
+            {subTitle}
+          </Typography>
+        ) : null}
+      </S.CenterContent>
+    </S.Container>
+  );
+};
 
 export default Header;
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -6,4 +6,23 @@ export const Container = styled.View`
   background: ${({ theme }) => theme.colors.white};
   border-bottom-width: 1px;
   border-bottom-color: #f4f4f4;
+  align-items: flex-end;
+  flex-direction: row;
+  padding: 20px;
+`;
+
+export const CenterContent = styled.View`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  bottom: 5px;
+  left: 20px;
+  align-items: center;
+  justify-content: center;
+  z-index: 0;
+`;
+
+export const ButtonIcon = styled.TouchableOpacity`
+  position: relative;
+  z-index: 1;
 `;
