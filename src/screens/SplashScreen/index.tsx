13fd96195093df0ed68b6840f2db56d0982e4cb1
Marcelo Barbosa <t_marcelo.barbosa@hapvida.com.br>
2023-01-31 14:07:54 -0300

    feature: create loading screen in splash with redirect

--- src/screens/SplashScreen/index.tsx
+++ src/screens/SplashScreen/index.tsx
@@ -1,3 +1,24 @@
-const SplashScreen = () => <></>;
+import React from 'react';
+
+import useUserStore from '#hooks/useStore';
+
+import { Loading } from '#components';
+
+import * as S from './styles';
+
+const SplashScreen = ({ navigation }: PageProps) => {
+  const isLogged = useUserStore((state) => state.isLogged);
+
+  React.useEffect(() => {
+    console.log(isLogged);
+    return navigation.navigate(isLogged ? 'tabs' : 'signin');
+  }, [isLogged]);
+
+  return (
+    <S.Container>
+      <Loading size="large" />
+    </S.Container>
+  );
+};
 
 export default SplashScreen;
