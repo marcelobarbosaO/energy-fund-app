b0818e32fb2ae8817ce9c88dec8dd1bfb8193fc1
Marcelo Barbosa <t_marcelo.barbosa@hapvida.com.br>
2023-01-31 15:20:18 -0300

    refactor: add new props

--- src/components/Typography/styles.ts
+++ src/components/Typography/styles.ts
@@ -1,14 +1,25 @@
-import styled from 'styled-components/native';
+import styled, { css } from 'styled-components/native';
 import { Text, TextProps } from 'react-native-paper';
 
 export interface TypographyProps extends Omit<TextProps, 'theme'> {
   children: React.ReactNode;
   fontSize?: number;
   bold?: boolean;
+  grey?: boolean;
+  centerAlign?: boolean;
 }
 
 export const Typography = styled(Text)<TypographyProps>`
   font-family: ${({ bold }) => (bold ? 'Sora_600SemiBold' : 'Sora_400Regular')};
   font-weight: ${({ bold }) => (bold ? 600 : 400)};
-  font-size: ${({ fontSize }) => fontSize || 14}px;
+  align-self: ${({ centerAlign }) => (centerAlign ? 'center' : 'auto')};
+  color: ${({ grey, theme }) =>
+    grey ? theme.colors.labelSection : theme.colors.black};
+
+  ${({ fontSize }) => {
+    if (fontSize)
+      return css`
+        font-size: ${fontSize}px;
+      `;
+  }};
 `;
