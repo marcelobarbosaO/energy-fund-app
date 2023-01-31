62f0b0a661b5a0e2a3e13eb6d0443b3969a87fe6
Marcelo Barbosa <t_marcelo.barbosa@hapvida.com.br>
2023-01-31 19:48:39 -0300

    feat: create file mock data

---
+++ src/@types/mock.d.ts
@@ -0,0 +1,10 @@
+declare interface Fund {
+  id: number;
+  name: string;
+  sigla: string;
+  value: string;
+  percent: string;
+  positive: boolean;
+  icon: React.ReactNode;
+  image: React.ReactNode;
+}
