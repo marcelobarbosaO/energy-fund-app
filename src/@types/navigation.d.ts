fba724ca56d334d44a86e78cab9e57de506259c6
Marcelo Barbosa <t_marcelo.barbosa@hapvida.com.br>
2023-01-31 18:34:48 -0300

    refactor: add type for assetDetail screen

--- src/@types/navigation.d.ts
+++ src/@types/navigation.d.ts
@@ -4,6 +4,21 @@ declare type AppStackNavigatorParamList = {
   splash: undefined;
   signin: undefined;
   register: undefined;
+  assetDetail: {
+    item: {
+      id: number;
+      name: string;
+      value: string;
+      percent: string;
+      positive: boolean;
+      icon: React.ReactNode;
+      image: React.ReactNode;
+    };
+  };
 };
 
 declare type PageProps = NativeStackScreenProps<RootStackParamList, 'splash'>;
+declare type AssetDetailProps = NativeStackScreenProps<
+  RootStackParamList,
+  'assetDetail'
+>;
