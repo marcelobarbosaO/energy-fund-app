8d56b77e0815eb9853a5d0a62035cd260955c7ba
Marcelo Barbosa <t_marcelo.barbosa@hapvida.com.br>
2023-01-31 14:04:04 -0300

    feature: create typography component

---
+++ src/components/Typography/index.tsx
@@ -0,0 +1,7 @@
+import * as S from './styles';
+
+const Typography = ({ children }: { children: React.ReactNode }) => (
+  <S.Typography>{children}</S.Typography>
+);
+
+export default Typography;
---
+++ src/components/Typography/styles.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components/native';
+import { Text } from 'react-native-paper';
+
+export const Typography = styled(Text)`
+  font-family: 'Sora_400Regular';
+`;
